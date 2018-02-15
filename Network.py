93c46683d8e7cacbc4d8e837a69750a27e0d9b6c
Developpix <developpix@gmail.com>
2018-02-15 14:35:51 +0100

    Add the method backpropagation for backpropagation.

--- Network.py
+++ Network.py
@@ -1,4 +1,5 @@
 from Layer import Layer
+from functions import *
 
 ######################################################################
 #
@@ -92,4 +93,104 @@ class Network:
 			for j in range(0, len(self.get_Layer(i).get_neuronsList())):
 				
 				self.get_Layer(i).get_Neuron(j).set_inputsList(inputsList)
+	
+	def backpropagation(self, normalOutputs, learningRate, alpha):
+		
+		"""
+			# The method for backpropagation
+		"""
+		
+		# Calculation of the error of the neurons in the last layer
+		# Check if the number of outputs is correct
+		if (len(normalOutputs) == len(self.get_lastLayer().get_neuronsList())):
+			
+			
+			for i in range(0, len(self.get_Layer(self._nbLayers-1).get_neuronsList())):
+				
+				# store the neuron i in a variable
+				n = self.get_Layer(self._nbLayers-1).get_Neuron(i)
+				
+				# Create an int to store the total
+				total = 0
+				
+				# Calculation of the total
+				for j in range(0, len(n.get_weightList())):
+					
+					total += n.get_weightList()[j] * n.get_inputsList()[j]
+				
+				# Get the derived function
+				if (n.get_nameActivationFunction() == 'sigmoide'):
+					derivedFunction = derived_sigmoide
+				elif (n.get_nameActivationFunction() == 'hyperbolicTangent'):
+					derivedFunction = derived_hyperbolicTangent
+				elif (n.get_nameActivationFunction() == 'identity'):
+					derivedFunction = derived_identity
+				
+				# Calculation of neuron's error and set his error
+				error = derivedFunction(total) * (normalOutputs[i] - n.get_output())
+				n.set_error(error)
+		else:
+			print('Le nombre de résultats attendus ne correspond pas aux sorties de la dernière couche')
+		
+		# Disseminate the error backwards
+		for i in range(1, self._nbLayers):
+			
+			# reverse the direction
+			i = self._nbLayers - (i + 1)
+			
+			# Neurons in the layer n
+			for j in range(0, len(self.get_Layer(i).get_neuronsList())):
+				
+				# Store the neuron in a variable
+				n = self.get_Layer(i).get_Neuron(j)
+				
+				# Create an int to store the total
+				total = 0
+				
+				# Neurons in the layer n+1
+				for k in range(0, len(self.get_Layer(i+1).get_neuronsList())):
+					
+					upperNeuron = self.get_Layer(i+1).get_Neuron(k)
+					total += upperNeuron.get_weightList()[j] * upperNeuron.get_error()
+				
+				# Create an int to store the total of inputs
+				inputsTotal = 0
+				
+				# Calculation of the total of inputs
+				for k in range(0, len(n.get_weightList())):
+					
+					inputsTotal += n.get_weightList()[k] * n.get_inputsList()[k]
+				
+				# Get the derived function
+				if (n.get_nameActivationFunction() == 'sigmoide'):
+					derivedFunction = derived_sigmoide
+				elif (n.get_nameActivationFunction() == 'hyperbolicTangent'):
+					derivedFunction = derived_hyperbolicTangent
+				elif (n.get_nameActivationFunction() == 'identity'):
+					derivedFunction = derived_identity
+				
+				# Calculation of neuron's error
+				error = derivedFunction(inputsTotal) * total
+				n.set_error(error)
+		
+		# Update weight thanks to inertia
+		for i in range(0, self._nbLayers):
+			
+			# Get neurons in the layer
+			for j in range(0, len(self.get_Layer(i).get_neuronsList())):
+				
+				# Store the neuron in a variable
+				n = self.get_Layer(i).get_Neuron(j)
+				
+				# Create a variable to store the new list of weight for the neuron
+				newWeightList = []
+				
+				# Get the new list of weight for the neuron
+				for k in range(0, len(n.get_weightList())):
+					
+					newWeightList.append(n.get_lastWeightList()[k] + (alpha * learningRate * n.get_error() * n.get_inputsList()[k]) + ((1 - alpha) * (n.get_weightList()[k] - n.get_lastWeightList()[k])))
+				
+				# Set the new list of weight
+				n.set_weightList(newWeightList)
+	
 	
